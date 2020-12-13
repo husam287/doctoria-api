5537c62425008594f4e441fd60293e6a6f9d0dbb
haidyelnahass <haidyelnahass@hotmail.com>
2020-12-13 07:22:47 +0200

    added view all doctors

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -1,6 +1,27 @@
-const getDateFromDay = require("../utils/getDateFromDay")
+const getDateFromDay = require("../utils/getDateFromDay");
+const Doctors = require('../models/doctors');
 
 exports.test = (req, res, next) => {
     console.log(getDateFromDay('Sunday'));
     next();
+}
+
+
+
+exports.viewAllDoctors = (req,res,next) => {
+//View all doctors in search.
+Doctors.find()
+    .then(doctors => {
+        //if doctors are found ,render them to the page.
+      res
+        .status(200)
+        .json({ message: 'Fetched posts successfully.', doctors: doctors });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+
 }
